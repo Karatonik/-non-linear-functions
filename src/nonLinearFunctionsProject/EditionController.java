4b84bb84b4401437195f506380decc879b8f88cf
MrKampla <mrkampla@gmail.com>
2019-04-24 13:21:34 +0200

    NAN

--- src/nonLinearFunctionsProject/EditionController.java
+++ src/nonLinearFunctionsProject/EditionController.java
@@ -1,4 +1,18 @@
 package nonLinearFunctionsProject;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+
 public class EditionController {
+
+    @FXML
+    Button showButton;
+    @FXML
+    TextField aField,bField,cField;
+
+    @FXML
+    public void onActionShowButton(){
+
+    }
 }
