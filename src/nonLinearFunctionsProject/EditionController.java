78f836678736e57f63ddeff17580390389281fa0
John Doe <mat.kalksztejn@wp.pl>
2019-04-27 15:50:46 +0200

    zarys

--- src/nonLinearFunctionsProject/EditionController.java
+++ src/nonLinearFunctionsProject/EditionController.java
@@ -1,11 +1,32 @@
 package nonLinearFunctionsProject;
 
 import javafx.fxml.FXML;
+
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 
-public class EditionController {
 
+public class EditionController  {
+    EditionController(){
+        switch (MainController.select){
+            case 1:
+            textFunction.setText("sinus = a * sin(b * x) + c ");
+            break;
+            case 2:
+                textFunction.setText("exponenital = ax^b + c ");
+                break;
+            case 3:
+                textFunction.setText(" pulse = xxx");
+                break;
+            case 4:
+                textFunction.setText("unit-line = xxx");
+
+        }
+    }
+
+    @FXML
+    Label textFunction;
     @FXML
     Button showButton;
     @FXML
@@ -13,6 +34,43 @@ public class EditionController {
 
     @FXML
     public void onActionShowButton(){
+        double a,b,c=1;
+        a=Double.valueOf(this.aField.getText());
+        b=Double.valueOf(this.bField.getText());
+        c=Double.valueOf(this.cField.getText()); //napiać regex
+        switch (MainController.select){
+            case 1:
+                displaySinGraph(a,b,c);
+                //okno funkcji sinus
+                break;
+            case 2:
+                displayExpGraph(a,b,c);
+                //okno funkcji exp
+                break;
+            case 3:
+                displayPulseGraph(a,b,c);
+                //okno funckji pulse
+                break;
+            case 4:
+                displayUnitGraph(a,b,c);
+                //okno funckji unite
+break;
+        }
+    }
+
+
+
+
+    public void displaySinGraph(double a,double b,double c){
+
+    }
+    public void displayExpGraph(double a,double b,double c){
+
+    }
+    public void displayPulseGraph(double a,double b,double c){
+
+    }
+    public void displayUnitGraph(double a,double b,double c){
 
     }
 }
--- src/nonLinearFunctionsProject/MainController.java
+++ src/nonLinearFunctionsProject/MainController.java
@@ -1,30 +1,68 @@
 package nonLinearFunctionsProject;
 
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.layout.Pane;
+import javafx.stage.Stage;
 
+import java.io.IOException;
 public class MainController {
 
     @FXML
     Button sinButton,expButton,pulseButton,unitButton;
-
+//Zmienna nadzorujaca wybrana funkcje
+   public static int select=0;
     @FXML
     public void onActionSinButton(){
-
+        select=1;
+        Stage stage = (Stage) this.sinButton.getScene().getWindow();
+        stage.close();
+        onActionButtons();
     }
+
 
     @FXML
     public void onActionExpButton(){
-
+        select=2;
+        Stage stage = (Stage) this.expButton.getScene().getWindow();
+        stage.close();
+        onActionButtons();
     }
 
     @FXML
     public void onActionPulseButton(){
-
+        select=3;
+        Stage stage = (Stage) this.pulseButton.getScene().getWindow();
+        stage.close();
+        onActionButtons();
     }
 
     @FXML
     public void onActionUnitButton(){
+        select=4;
+        Stage stage = (Stage) this.unitButton.getScene().getWindow();
+        stage.close();
+        onActionButtons();
+    }
 
+
+    public void onActionButtons(){
+        try {
+            Stage newStage = new Stage();
+            FXMLLoader newfxmlLoader = new FXMLLoader();
+            Pane root = newfxmlLoader.load(getClass().getResource("editionWindow.fxml").openStream());
+            EditionController editionController = newfxmlLoader.getController();
+
+            Scene newscene = new Scene(root);
+
+            newStage.setScene(newscene);
+            newStage.setTitle("Edition");
+            newStage.setResizable(false);
+            newStage.show();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
