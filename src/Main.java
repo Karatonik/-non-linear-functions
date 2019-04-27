63cdacb8d2d033ea7a9f1fd5334bb8643917429d
John Doe <mat.kalksztejn@wp.pl>
2019-04-27 14:11:11 +0200

    check

--- src/Main.java
+++ src/Main.java
@@ -7,6 +7,9 @@ import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 public class Main extends Application {
+    public static void main(String[] args) {
+        launch(args);
+    }
 
     @Override
     public void start(Stage primaryStage) throws Exception{
@@ -14,10 +17,5 @@ public class Main extends Application {
         primaryStage.setTitle("Non-linear functions");
         primaryStage.setScene(new Scene(root, 500, 400));
         primaryStage.show();
-    }
-
-
-    public static void main(String[] args) {
-        launch(args);
     }
 }
